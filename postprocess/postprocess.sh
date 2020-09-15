1303193a00a30eeb7ea4d71288ec1c257a4b9edd
David Rogers <dhrogers@login3.summit.olcf.ornl.gov>
2020-09-15 17:27:22 -0400

    updating message

--- postprocess/postprocess.sh
+++ postprocess/postprocess.sh
@@ -4,6 +4,6 @@ source ./pantheon/env.sh > /dev/null 2>&1
 
 echo "----------------------------------------------------------------------"
 echo "PTN: Post-processing" 
-echo "     (no operations"
+echo "     (no operations)"
 echo "----------------------------------------------------------------------"
 
