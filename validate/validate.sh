59b2341236d8972da202532e0df5c206739f86a4
David Rogers <dhrogers@login2.summit.olcf.ornl.gov>
2020-09-17 15:27:12 -0400

    updating validation

--- validate/validate.sh
+++ validate/validate.sh
@@ -5,22 +5,28 @@ source ./pantheon/env.sh > /dev/null 2>&1
 echo "----------------------------------------------------------------------"
 echo "PTN: validating" 
 
-OUTPUT=$PANTHEON_RUN_DIR/cinema_databases/clover_db/data.csv
-GOLD=validate/data/cloverleaf3d_001.cdb/data.csv
+OUTPUT=$PANTHEON_RUN_DIR/cinema_databases/clover_db
+GOLD=validate/data/cloverleaf3d_001.cdb
 
 echo "     $OUTPUT"
 
-PASS=false
-if [ -f $OUTPUT ]; then
-    if cmp "$OUTPUT" "$GOLD"; then
-        PASS=true
-    else
-        echo "FILES differ:"
-        echo "    $OUTPUT"
-        echo "    $GOLD"
-    fi
+dirs="0.0 9.0 19.0"
+img="0.0_0.0_clover_db.png"
+
+PASS=true
+if [ -d $OUTPUT ]; then
+    for val in $dirs; do
+        if cmp "$OUTPUT/$val/$img" "$GOLD/$val/$img"; then
+            NOOP=true
+        else
+            echo "FILES differ:"
+            echo "    $OUTPUT"
+            echo "    $GOLD"
+            PASS=false
+        fi
+    done
 else
-    echo "FILE: $OUTPUT does not exist"
+    echo "Cinema Database: $OUTPUT does not exist"
 fi
 
 if $PASS; then
